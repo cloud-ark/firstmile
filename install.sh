09480318ef9426206af5a31dcafa6cbad20cb04b
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2017-01-19 10:30:03 -0600

    Activating correct virtenv in install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@ echo "Creating test virtual environment"
 pip install virtualenv
 virtenv=test-cld
 virtualenv $virtenv
-source test-lme/bin/activate
+source $virtenv/bin/activate
 
 echo "Installing client"
 cd client
 sudo python setup.py install
+cd ..
 
 echo "Starting server. Server logs stored in cld-server.log"
 ps -eaf | grep 'python cld.py' | awk '{print $2}' | xargs kill
-cd ..
 pip install -r requirements.txt
 nohup python cld.py 1>>cld-server.log 2>&1 &
