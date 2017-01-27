2ee0bd75884d04eedc45a85c2d4b1ff8dbd65288
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2017-01-27 16:27:29 -0600

    Functionaltests - Update to test_local
    - Updated test_local (method name change)
    - Moved some functions from test_local to utils
      as they will be used from other test modules
      as well

--- functionaltests/local/test_local.py
+++ functionaltests/local/test_local.py
@@ -6,9 +6,10 @@ Created on Jan 4, 2017
 import os
 import subprocess
 import pexpect
-import time
 
 from testtools import TestCase
+
+from functionaltests import utils
 
 MAX_WAIT_COUNT = 180
 SAMPLE_REPO = "https://devdattakulkarni@bitbucket.org/devdattakulkarni/lme-examples.git"
@@ -24,51 +25,7 @@ class TestLocal(TestCase):
                                                                              sample_repo_name=SAMPLE_REPO_NAME)
             os.system(cmd)
 
-    def _parse_dep_id(self, cloud, lines):
-        print("Lines:%s" % lines)
-        for line in lines.split("\n"):
-            print("Line:%s" % line)
-            parts = line.split("|")
-            parts = [item.rstrip() for item in parts]
-            parts = [item.lstrip() for item in parts]
-            if parts and cloud in parts:
-                name = parts[1].rstrip().lstrip()
-                dep_id = parts[2].rstrip().lstrip()
-                print("Dep id:%s Name:%s" % (dep_id, name))
-                return dep_id, name
-
-    def _assert_deploy_complete(self, show_cmd, status):
-        count = 0
-        while count < MAX_WAIT_COUNT:
-            try:
-                output = subprocess.Popen(show_cmd,
-                                          stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE,
-                                          shell=True).communicate()[0]
-                print("Show output:%s" % output)
-            except Exception as e:
-                print(e)
-
-            for line in output.split("\n"):
-                parts = line.split(" ")
-                if status in parts:
-                    return True
-
-            count = count + 1
-            time.sleep(1)
-        return False
-
-    def _cleanup(self, name):
-        cmd = ("docker ps | grep {name} | head -1 | awk '{{print $1}}' | xargs docker stop").format(name=name)
-        os.system(cmd)
-        
-        cmd = ("docker images | grep {name} | head -1 | awk '{{print $3}}' | xargs docker rmi -f").format(name=name)
-        os.system(cmd)
-        
-        cmd = ("docker ps -a | grep {name} | head -1 | awk '{{print $1}}' | xargs docker rm").format(name=name)
-        os.system(cmd)
-
-    def test_with_flags_no_service(self):
+    def test_app_deploy_no_service(self):
         cwd = os.getcwd()
         os.chdir("/tmp/lme-examples/hello-world")
         deploy_cmd = "cld app deploy --cloud local-docker"
@@ -81,18 +38,50 @@ class TestLocal(TestCase):
             child.expect(expected)
             child.sendline("5000")
             lines = child.read()
-            dep_id, name = self._parse_dep_id("local-docker", lines)
+            dep_id, name = utils.parse_dep_id("local-docker", lines)
         except Exception as e:
             print(e)
 
         show_cmd = ("cld app show --deploy-id {dep_id}").format(dep_id=dep_id)
-        self.assertTrue(self._assert_deploy_complete(show_cmd, "APP_DEPLOYMENT_COMPLETE"),
+        self.assertTrue(utils.assert_deploy_complete(show_cmd, "APP_DEPLOYMENT_COMPLETE"),
                         "App deployment completed")
-        self._cleanup(name)
+        utils.cleanup(name)
 
         os.chdir(cwd)
 
-    def test_mysql_instance_deploy(self):
+    def test_app_deploy_with_mysql_service(self):
+        cwd = os.getcwd()
+        os.chdir("/tmp/lme-examples/greetings-python")
+        deploy_cmd = "cld app deploy --cloud local-docker --service-name mysql"
+        dep_id = ''
+        try:
+            child = pexpect.spawn(deploy_cmd)
+            expected = ">"
+            child.expect(expected)
+            child.sendline("application.py")
+            child.expect(expected)
+            child.sendline("5000")
+            child.expect(expected)
+            child.sendline("DB")
+            child.expect(expected)
+            child.sendline("HOST")
+            child.expect(expected)
+            child.sendline("USER")
+            child.expect(expected)
+            child.sendline("PASSWORD")
+            lines = child.read()
+            dep_id, name = utils.parse_dep_id("local-docker", lines)
+        except Exception as e:
+            print(e)
+
+        show_cmd = ("cld app show --deploy-id {dep_id}").format(dep_id=dep_id)
+        self.assertTrue(utils.assert_deploy_complete(show_cmd, "APP_DEPLOYMENT_COMPLETE"),
+                        "App deployment completed")
+        utils.cleanup(name)
+
+        os.chdir(cwd)
+
+    def test_mysql_instance_provision(self):
         cwd = os.getcwd()
         os.chdir("/tmp/lme-examples/greetings-python")
         deploy_cmd = "cld service provision --cloud local-docker"
@@ -101,13 +90,13 @@ class TestLocal(TestCase):
             output = subprocess.Popen(deploy_cmd, stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE, shell=True).communicate()[0]
 
-            dep_id, name = self._parse_dep_id("local-docker", output)
+            dep_id, name = utils.parse_dep_id("local-docker", output)
         except Exception as e:
             print(e)
 
         show_cmd = ("cld service show --deploy-id {dep_id}").format(dep_id=dep_id)
-        self.assertTrue(self._assert_deploy_complete(show_cmd, "SERVICE_DEPLOYMENT_COMPLETE"),
+        self.assertTrue(utils.assert_deploy_complete(show_cmd, "SERVICE_DEPLOYMENT_COMPLETE"),
                         "Service deployment completed")
-        self._cleanup(name)
+        utils.cleanup(name)
 
         os.chdir(cwd)
---
+++ functionaltests/utils.py
@@ -0,0 +1,58 @@
+'''
+Created on Jan 27, 2017
+
+@author: devdatta
+'''
+
+import os
+import subprocess
+import time
+
+MAX_WAIT_COUNT = 180
+SAMPLE_REPO = "https://devdattakulkarni@bitbucket.org/devdattakulkarni/lme-examples.git"
+SAMPLE_REPO_NAME = "lme-examples"
+
+
+def parse_dep_id(cloud, lines):
+    #print("Lines:%s" % lines)
+    for line in lines.split("\n"):
+        #print("Line:%s" % line)
+        parts = line.split("|")
+        parts = [item.rstrip() for item in parts]
+        parts = [item.lstrip() for item in parts]
+        if parts and cloud in parts:
+            name = parts[1].rstrip().lstrip()
+            dep_id = parts[2].rstrip().lstrip()
+            print("Dep id:%s Name:%s" % (dep_id, name))
+            return dep_id, name
+
+def assert_deploy_complete(show_cmd, status):
+    count = 0
+    while count < MAX_WAIT_COUNT:
+        try:
+            output = subprocess.Popen(show_cmd,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      shell=True).communicate()[0]
+            #print("Show output:%s" % output)
+        except Exception as e:
+            print(e)
+
+        for line in output.split("\n"):
+            parts = line.split(" ")
+            if status in parts:
+                return True
+
+        count = count + 1
+        time.sleep(1)
+    return False
+
+def cleanup(name):
+    cmd = ("docker ps | grep {name} | head -1 | awk '{{print $1}}' | xargs docker stop").format(name=name)
+    os.system(cmd)
+    
+    cmd = ("docker images | grep {name} | head -1 | awk '{{print $3}}' | xargs docker rmi -f").format(name=name)
+    os.system(cmd)
+    
+    cmd = ("docker ps -a | grep {name} | head -1 | awk '{{print $1}}' | xargs docker rm").format(name=name)
+    os.system(cmd)
