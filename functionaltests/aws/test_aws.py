b715342aa635e1ab727e4c577132a5c9c3ad1c9b
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2017-01-28 18:34:52 -0600

    Functionaltests AWS
    Added tests for app_with_service and service provisioning

--- functionaltests/aws/test_aws.py
+++ functionaltests/aws/test_aws.py
@@ -49,3 +49,57 @@ class TestAWS(TestCase):
                         "App deployment completed")
         utils.cleanup(name)
         os.chdir(cwd)
+
+    def test_app_deploy_with_mysql_service(self):
+        cwd = os.getcwd()
+        os.chdir("/tmp/lme-examples/greetings-python")
+        deploy_cmd = "cld app deploy --cloud aws --service-name mysql"
+        dep_id = ''
+        try:
+            child = pexpect.spawn(deploy_cmd)
+            expected = ">"
+            child.expect(expected)
+            child.sendline("application.py")
+            child.expect(expected)
+            child.sendline("5000")
+            child.expect(expected)
+            child.sendline("DB")
+            child.expect(expected)
+            child.sendline("HOST")
+            child.expect(expected)
+            child.sendline("USER")
+            child.expect(expected)
+            child.sendline("PASSWORD")
+            lines = child.read()
+            dep_id, name = utils.parse_dep_id("aws", lines)
+        except Exception as e:
+            print(e)
+
+        show_cmd = ("cld app show --deploy-id {dep_id}").format(dep_id=dep_id)
+        self.assertTrue(utils.assert_deploy_complete(show_cmd, "APP_DEPLOYMENT_COMPLETE",
+                                                     wait_count=900),
+                        "App deployment completed")
+
+        utils.cleanup(name)
+        os.chdir(cwd)
+
+    def test_mysql_instance_provision(self):
+        cwd = os.getcwd()
+        os.chdir("/tmp/lme-examples/greetings-python")
+        deploy_cmd = "cld service provision --cloud aws"
+        dep_id = ''
+        try:
+            output = subprocess.Popen(deploy_cmd, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, shell=True).communicate()[0]
+
+            dep_id, name = utils.parse_dep_id("aws", output)
+        except Exception as e:
+            print(e)
+
+        show_cmd = ("cld service show --deploy-id {dep_id}").format(dep_id=dep_id)
+        self.assertTrue(utils.assert_deploy_complete(show_cmd, "SERVICE_DEPLOYMENT_COMPLETE",
+                                                     wait_count=MAX_WAIT_COUNT),
+                        "Service deployment completed")
+        utils.cleanup(name)
+
+        os.chdir(cwd)
